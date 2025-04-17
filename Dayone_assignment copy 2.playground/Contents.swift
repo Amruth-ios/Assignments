b5a0307bcbcbf803e3a45e26f2ee11fedcdef59c
Amruth Kallyam <amruth.kallyam@techconsulting.tech>
2025-04-17 15:24:33 -0400

    First Commit

---
+++ Dayone_assignment copy 2.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+/*Assignment Questions:
+
+ 1. what is diff b/w var and let
+Ans : The difference between var and let :
+
+var : Its mutable
+let : Its immutable
+
+ 2. how to create mutalbe and immutalbe varialbes
+ 
+ Mutable:   var color = "Black";
+            print(color)
+ 
+ Immutable:  let pi = 3.14;
+             print("pi value is : \(pi)")
+
+
+ 3. diff b/w explict and implict variables
+ 
+ 
+ 
+ 4. how to create optioanl variables
+ 
+ 
+ 5. what is optioanl binding
+ 
+ 
+ 6. how to unwrap optional variable
+ 
+ 
+ 7. diff b/w nil colasing, if let and guard let
+ 
+ 
+ 8. diff b/w array and dict and set
+ 
+ 
+ 9, can you assing a optional value to non optional variable , if yes how , if now why?
+*/
+
+let pi = 3.14;
+print("pi value is : \(pi)")
+
+var color = "Black";
+          print(color)
